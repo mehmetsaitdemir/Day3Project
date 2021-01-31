f5d39cd2f4fd56d50d0399b9e210887433f1674b
Mehmet Sait DEMİR <demirmsait@gmail.com>
2021-01-31 12:28:32 +0200

    31.01.2021

--- src/test/java/MehmetSait/Hello.java
+++ src/test/java/MehmetSait/Hello.java
@@ -1,4 +1,7 @@
 package MehmetSait;
 
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Clone");
+    }
 }
---
+++ src/test/java/NewYear2021.java
@@ -0,0 +1,5 @@
+public class NewYear2021 {
+    public static void main(String[] args) {
+        System.out.println("Hello fom Germany");
+    }
+}
